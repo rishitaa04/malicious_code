d0420938f023bf395751b9c4a04ee4d5499f3db5
Rishita <125037556+rishitaa04@users.noreply.github.com>
2025-11-26 16:53:19 +0530

    Updated package.json

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to React</h1>
+        <p>Your application is ready to go.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
